0f285733f3567d3ab00beb5c45d0b4104905965a
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-05-04 23:14:04 +0200

    aggiunto dettaglio

--- resources/views/partials/list.blade.php
+++ resources/views/partials/list.blade.php
@@ -1,8 +1,8 @@
 <h1>Ecco i nostri film:</h1>
 <ul class="list-group">
     @foreach ($movie_list as $movie)
-        <li class="list-group-item list-group-item-dark">
-            <a class="text-decoration-none fs-5" href="{{route('detail', $movie->id)}}">{{$movie -> title}}</a>
+        <li class="list-group-item list-group-item-dark {{ Str::endsWith(url()->current(),$movie->id) ? 'active' : '' }}">
+            <a class="text-decoration-none fs-5 {{ Str::endsWith(url()->current(),$movie->id) ? 'text-light' : '' }}" href="{{route('detail', $movie->id)}}">{{$movie -> title}}</a>
         </li>
     @endforeach
 </ul>
