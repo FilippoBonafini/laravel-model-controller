7cbf41488aa13f921d15c7aa30286091019f70ee
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-05-05 10:30:10 +0200

    .

--- resources/views/partials/list.blade.php
+++ resources/views/partials/list.blade.php
@@ -1,10 +1,16 @@
 <h1>Ecco i nostri film:</h1>
 <ul class="list-group">
-    
+
     @foreach ($movie_list as $movie)
-    {{-- {{dd(urlencode($movie->title))}} --}}
-        <li class="list-group-item list-group-item-dark {{ Str::endsWith(url()->current(), urlencode($movie->title)) ? 'active' : '' }}">
-            <a class="text-decoration-none fs-5 {{ Str::endsWith(url()->current(),urlencode($movie->title)) ? 'text-light' : '' }}" href="{{route('detail', ['id' => $movie->id, 'title' => urlencode($movie->title)])}}">{{$movie -> title}}</a>
+        {{-- {{dd(urlencode($movie->title))}} --}}
+        <li
+            class="list-group-item list-group-item-dark 
+        {{ Str::endsWith(url()->current(), urlencode($movie->title)) ? 'active' : '' }}">
+            <a class="text-decoration-none fs-5 
+            {{ Str::endsWith(url()->current(), urlencode($movie->title)) ? 'text-light' : '' }}"
+                href="{{ route('detail', ['id' => $movie->id, 'title' => urlencode($movie->title)]) }}">
+                {{ $movie->title }}
+            </a>
         </li>
     @endforeach
 </ul>
